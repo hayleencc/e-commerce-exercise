07146daf5d6f92d4787716bac1fd094ee8f666e2
Hayleen Carrillo <hayleen.carrillo@gmail.com>
2024-01-28 22:20:35 -0500

    fix: correct render size for all product card

--- src/assets/CartLogo/index.jsx
+++ src/assets/CartLogo/index.jsx
@@ -1,6 +1,6 @@
 import './CartLogo.css'
 
-function CartLogo () {
+function CartLogo() {
     return (
         <div className='cartLogoContainer'>
             <img src='src/assets/CartLogo/shopping-cart.png'/>
