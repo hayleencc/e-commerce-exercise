39fd80e4f2bd56cf5c9b81bb214846fe5744794b
Hayleen Carrillo <hayleen.carrillo@gmail.com>
2024-01-29 01:02:10 -0500

    fix: add filter by price functionality

--- src/components/ResultInfoBar/SortFilter/index.jsx
+++ src/components/ResultInfoBar/SortFilter/index.jsx
@@ -1,9 +1,18 @@
 import './SortFilter.css'
+import { useContext } from 'react';
+import { SearchContext } from '../../../contexts/SearchContext';
 
 function SortFilter () {
+    const {
+        setOrder,
+    } = useContext(SearchContext);
+    const handleSetOrderChange = (event) => {
+        setOrder(event.target.value);
+    }
+
     return (
         <div className='SortFilterContainer'>
-            <select name="order" id="order">
+            <select name="order" id="order" onChange={handleSetOrderChange} > 
                 <option value="Name">Name</option>
                 <option value="Price_Low">Price: Low to High</option>
                 <option value="Price_High">Price: High to Low</option>
